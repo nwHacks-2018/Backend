dba48c933263af028c82da10db1958d08acd39b6
Lara Rode <lara.rode@hotmail.com>
2018-01-13 16:51:12 -0800

    Add to User schema, Add chat and conversation models and controllers

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRoutes from './user.route';
 import authRoutes from './auth.route';
+import chatRoutes from './chat.route';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
@@ -14,5 +15,8 @@ router.use('/users', userRoutes);
 
 // mount auth routes at /auth
 router.use('/auth', authRoutes);
+
+// mount chat routes at /chat
+router.use('/chat', chatRoutes);
 
 export default router;
